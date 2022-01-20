9804cf0ba51dd70b96c1fe9d6c55746de1f486f3
Esin KARAN <esinkarann@gmail.com>
2022-01-21 00:31:23 +0300

    Initial Commit

---
+++ ArtGallery/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ArtGallery
+//
+//  Created by Esin Karan on 21.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
